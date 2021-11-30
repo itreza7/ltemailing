949a0e52789c3465285bc5be059247414905b0d7
itreza7 <itreza7@gmail.com>
2021-11-30 17:23:09 +0330

    Larateam mailing system modify

--- app/Mails/StyleMail.php
+++ app/Mails/StyleMail.php
@@ -12,19 +12,19 @@ class StyleMail extends Mailable
 {
     use Queueable, SerializesModels;
 
-    private $template;
-    private $subject;
-    private $content;
+    public $mail_template;
+    public $mail_subject;
+    public $mail_content;
 
     public function __construct($template, $subject, $content)
     {
-        list($this->template, $this->subject, $this->content) = [$template, $subject, $content];
+        list($this->mail_template, $this->subject, $this->mail_content) = [$template, $subject, $content];
     }
 
     public function build()
     {
-        list($template, $subject, $content) = [$this->template, $this->subject, $this->content];
-
-        return $this->view('admincp::mails.reset-password');
+        list($template, $subject, $content) = [$this->mail_template, $this->mail_subject, $this->mail_content];
+        $template = 'ltmailing::' . $template;
+        return $this->markdown($template);
     }
 }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,25 @@
 
 namespace Larateam\Emailing\Providers;
 
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
+    private const MAILING_DIR = __DIR__ . '/../../';
+
+    public function boot()
+    {
+        if (!File::exists(config_path('ltmailing.php'))) {
+            $this->mergeConfigFrom(self::MAILING_DIR . 'config/ltmailing.php', 'ltmailing');
+        }
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                self::MAILING_DIR . 'config/ltmailing.php' => config_path('ltmailing.php'),
+            ], 'config');
+        }
+        $this->loadTranslationsFrom(self::MAILING_DIR . 'resources/lang', 'ltmailing');
+        $this->loadViewsFrom(self::MAILING_DIR . 'resources/views', 'ltmailing');
+    }
 
 }
